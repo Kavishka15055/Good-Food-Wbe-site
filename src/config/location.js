61a8ebeb8a581f3660dd95369c955739c0ec276b
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-16 15:55:48 +0530

    Create location.js

---
+++ src/config/location.js
@@ -0,0 +1,10 @@
+export const restaurantConfig = {
+  coordinates: {
+    lat: 6.708552,  // Change this
+    lng: 80.787876   // Change this
+  },
+  address: "#110, Y junction, Belihuloya, Sri lanka",
+  phone: "+91 964325789",
+  hours: "8:00 AM - 10:00 PM",
+  zoomLevel: 15
+};
