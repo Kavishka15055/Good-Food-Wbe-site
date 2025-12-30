59b9b62ef83109734bd6eff05d1be65fb39648bc
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-12-30 13:26:09 +0530

    move front end file in to frontend folder

---
+++ frontend/src/components/Contact/Contact.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { FaPhone } from 'react-icons/fa';
+import { IoLocationSharp } from 'react-icons/io5';
+import { MdEmail } from 'react-icons/md';
+
+function Contact() {
+    return (
+        <>
+            <div id="contact" className='text-white mt-20'>
+                <div className='w-[100%] mx-auto bg-gradient-to-b from-primary to-primaryDark rounded-t-3xl mr-150'>
+                    <div className='max-w-screen-xl mx-auto px-4'>
+                        <h1 className='py-10 text-3xl font-bold text-yellow text-center'>
+                            Contact Us
+                        </h1>
+                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 border-b-2 border-white pb-6'>
+                            {/* Address */}
+                            <div className='text-center space-y-4'>
+                                <div className='flex justify-center'>
+                                    <IoLocationSharp className='text-5xl' />
+                                </div>
+                                <p>
+                                    #110, Y junction, Belihuloya,<br />
+                                    Sri Lanka
+                                </p>
+                            </div>
+
+                            {/* Email */}
+                            <div className='text-center space-y-4'>
+                                <div className='flex justify-center'>
+                                    <MdEmail className='text-5xl' />
+                                </div>
+                                <div>
+                                    <p>info@goodfood.com</p>
+                                    <p>hr@goodfood.com</p>
+                                </div>
+                            </div>
+
+                            {/* Phone */}
+                            <div className='text-center space-y-4'>
+                                <div className='flex justify-center'>
+                                    <FaPhone className='text-5xl' />
+                                </div>
+                                <div>
+                                    <p>+91 964325789 - Sales and Services</p>
+                                    <p>+91 487951587 - Hiring Queries</p>
+                                    <p>+91 987458632 - Whatsapp</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Footer bottom */}
+                        <div className='flex flex-col md:flex-row justify-between items-center text-sm p-4'>
+                            <p>@ 2025 TCJ. All rights reserved</p>
+                            <div className='flex gap-6 mt-2 md:mt-0'>
+                                <a href="#">Privacy Policy</a>
+                                <a href="#">Terms & Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Contact;
---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { assets } from '../../assets/assets';
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const BgStyle = {
+    backgroundImage: `url(${assets.heroBg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    width: "100%",
+    height: "100%",
+};
+
+function Hero() {
+    return (
+        <div id='hero' style={BgStyle} className='relative z-[-1]  mt-[-15px]'>
+            <div className='container mx-auto px-4 py-16 sm:py-10'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 items-center gap-10'>
+
+                    {/* Text Section */}
+                    <div
+                        className='space-y-8 text-dark order-2 sm:order-1 text-left sm:text-left  w-[700px]'
+                        data-aos="fade-up"
+                    >
+                        <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold leading-tight '>
+                            Diverse Flavors, One Delivery.
+                        </h1>
+                        <h1 className='text-xl md:text-1xl lg:text-2xl font-bold leading-tight mt-[-35px]'>
+                            <span className='text-secondary text-2xl md:text-3xl'>Fresh meals</span>{" "}
+                            from our kitchen to your home
+                        </h1>
+
+                        <p className='text-lg md:text-xl text-gray-700 mt-[-35px]'>
+                            Gourmet delivery experience <br />
+                            From <span className='font-semibold'>LKR 2,500</span>/week
+                        </p>
+
+                        {/* Button - Removed data-aos from wrapper div */}
+                        {/* <div>
+                            <PrimaryButton />
+                        </div> */}
+                    </div>
+
+                    {/* Image Section */}
+                    <div
+                        className='order-1 sm:order-2 relative flex justify-center'
+                        data-aos="fade-up"
+                        data-aos-delay="500"
+                    >
+                        <img
+                            src={assets.hero}
+                            alt='Hero'
+                            className='w-[80%] sm:w-[100%] md:scale-110 sm:translate-y-8 max-w-[500px]'
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
