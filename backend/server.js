4abb2c210c5edecd5f3fe1a3578043871bed5cb3
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-12-31 17:06:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes - Make sure profileRoutes is registered
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend is working!" });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/profile", profileRoutes); 
